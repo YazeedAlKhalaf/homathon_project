cf5e630f4df93fa9d08275bd9bb91300c8a05edd
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-04-23 01:01:29 +0300

    :tada: Making The Map Screen (Part 1)

--- lib/src/ui/views/map_view.dart
+++ lib/src/ui/views/map_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:homathon_project/src/providers/map_provider.dart';
 import 'package:homathon_project/src/ui/shared/ui_helpers.dart';
+import 'package:homathon_project/src/ui/widgets/rounded_button.dart';
 import 'package:provider_architecture/provider_architecture.dart';
 
 class MapView extends StatefulWidget {
@@ -13,6 +15,11 @@ class _MapViewState extends State<MapView> {
   Widget build(BuildContext context) {
     return ViewModelProvider.withConsumer(
       viewModelBuilder: () => MapProvider(),
+      onModelReady: (MapProvider provider) {
+        provider.initialize(
+          contextFromFunc: context,
+        );
+      },
       builder: (
         BuildContext context,
         MapProvider provider,
@@ -22,12 +29,48 @@ class _MapViewState extends State<MapView> {
           appBar: AppBar(),
           body: SafeArea(
             child: Center(
-              child: Text(
-                'Map View',
-                style: TextStyle(
-                  fontSize: blockSizeHorizontal(context) * 7,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Stack(
+                children: <Widget>[
+                  GoogleMap(
+                    mapType: MapType.normal,
+                    trafficEnabled: provider.showTraffic,
+                    onMapCreated: provider.onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(
+                        40.5,
+                        60.7,
+                      ),
+                    ),
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: true,
+                    // onTap: (latLng) {
+                    //   provider.add(context, latLng);
+                    // },
+                    markers: Set<Marker>.of(provider.markers.values),
+                  ),
+
+                  // The Upper Layer
+                  Padding(
+                    padding: EdgeInsets.all(
+                      blockSizeHorizontal(context) * 3,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        RoundedButton(
+                          text: 'Traffic',
+                          onPressed: () {
+                            if (provider.showTraffic) {
+                              provider.setTraffic(false);
+                            } else {
+                              provider.setTraffic(true);
+                            }
+                          },
+                          fontSize: blockSizeHorizontal(context) * 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
