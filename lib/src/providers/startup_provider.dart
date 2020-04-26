b92590870128d65597e6f9e5daa1d501be448d20
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-04-26 15:42:33 +0300

    :tada: Adding Permission Handling!

--- lib/src/providers/startup_provider.dart
+++ lib/src/providers/startup_provider.dart
@@ -2,11 +2,68 @@ import 'dart:async';
 
 import 'package:homathon_project/src/constants/route_names.dart';
 import 'package:homathon_project/src/providers/base_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class StartupProvider extends BaseProvider {
   Future handleStartUpLogic() async {
-    bool hasLoggedInUser = await authenticationService.isUserLoggedIn();
+    switch (await Permission.location.status) {
+      case PermissionStatus.granted:
+        await handleLogin();
+        break;
+      case PermissionStatus.undetermined:
+        PermissionStatus permissionStatus = await Permission.location.request();
 
+        if (permissionStatus == PermissionStatus.granted) {
+          await handleLogin();
+        } else {
+          dialogService.showDialog(
+            title: 'OOPS!',
+            description: '''
+                You can\'t use the app without allowing the locations service.
+                Please relaunch the app or go to settings and allow location usage.
+                ''',
+          );
+        }
+        break;
+      case PermissionStatus.restricted:
+        dialogService.showDialog(
+          title: 'OOPS!',
+          description: '''
+                You can\'t use the app without allowing the locations service.
+                The location is restricted on your device, please tell you parent or administartor to allow it so that you can use the app.
+                ''',
+        );
+
+        break;
+      case PermissionStatus.denied:
+        PermissionStatus permissionStatus = await Permission.location.request();
+
+        if (permissionStatus == PermissionStatus.granted) {
+          await handleLogin();
+        } else {
+          dialogService.showDialog(
+            title: 'OOPS!',
+            description: '''
+                You can\'t use the app without allowing the locations service.\nPlease relaunch the app or go to settings and allow location usage.
+                ''',
+          );
+        }
+        break;
+      case PermissionStatus.permanentlyDenied:
+        dialogService.showDialog(
+          title: 'OOPS!',
+          description: '''
+                You can\'t use the app without allowing the locations service,
+                Please go to settings and allow location usage.
+                ''',
+        );
+        break;
+      default:
+    }
+  }
+
+  handleLogin() async {
+    bool hasLoggedInUser = await authenticationService.isUserLoggedIn();
     if (hasLoggedInUser) {
       navigationService.navigateToAndRemoveUntill(HomeViewRoute);
     } else {
